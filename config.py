2754cfc753689a1a0d91cd0ab64de09ced35ece3
Rimurutempest187 <karangmerry@gmail.com>
2026-02-22 20:40:43 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,1 @@
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-ADMINS = [int(x.strip()) for x in os.getenv('ADMINS','').split(',') if x.strip()]
-DB_FILE = os.getenv('DATABASE_FILE','data/churchbot.db')
-
-# Bot-visible footer
-FOOTER = "\n\nCreate by : PINLON-YOUTH"
-
-# Default group setting for messages-to-trigger-quiz
-DEFAULT_MESSAGES_TO_TRIGGER_QUIZ = 50
